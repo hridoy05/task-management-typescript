54962501603721255dca40665fead4985132f0a8
Hridoy Banik <hridoy.banik71@gmail.com>
2023-03-02 13:14:30 +0600

    add: express validator added

--- backend/src/tasks/tasks.router.ts
+++ backend/src/tasks/tasks.router.ts
@@ -1,6 +1,7 @@
+import { createValidator } from './tasks.validator';
 import { Router, Response, Request } from 'express';
 import { TaskController } from './tasks.controller';
-
+import { validationResult } from 'express-validator';
 export const tasksRouter: Router = Router();
 
 tasksRouter.get(
@@ -9,5 +10,19 @@ tasksRouter.get(
     const taskController = new TaskController();
     const allTasks = await taskController.getAll();
     res.json(allTasks).status(200);
+  },
+);
+
+tasksRouter.post(
+  '/tasks',
+  createValidator,
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
   },
 );
